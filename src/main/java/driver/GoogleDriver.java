bd4186b4389ab29cf1a4bd98909462526cfea66b
wangdada <862485538@qq.com>
2020-04-30 10:48:06 +0800

    新增设置新版浏览器去除"Chrome正在受自动化软件控制"警告

--- src/main/java/driver/GoogleDriver.java
+++ src/main/java/driver/GoogleDriver.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.Collections;
+
 public class GoogleDriver {
     public WebDriver driver = null;
 
@@ -12,15 +14,19 @@ public class GoogleDriver {
         System.setProperty("webdriver.chrome.driver", driverPath);
         //创建chrome浏览器的属性对象。
         ChromeOptions option = new ChromeOptions();
-        // 去除Chrome浏览器上的黄色警告
-        option.addArguments("--disable-infobars");
+        // 去除Chrome浏览器上的黄色警告,注释的两种方法在新版浏览器上已失效
+//        option.addArguments("--disable-infobars");
+//        option.addArguments("--test-type");
+        //新版本浏览器设置去除浏览器正在受自动化软件控制警告
+        option.setExperimentalOption("useAutomationExtension",false);
+        option.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
         //加载chrome用户文件
         option.addArguments("--user-data-dir=C:\\Users\\w\\AppData\\Local\\Google\\Chrome\\User Data");
         //最大化浏览器窗口
         option.addArguments("--start-maximized");
         try { // 创建一个 Chrome 的浏览器实例
             this.driver = new ChromeDriver(option);
-            // 让浏览器访问空白页
+            // 打开空白页
             driver.get("about:blank");
         } catch (Exception e) {
             AutoLogger.log.info("driver创建失败！请检查配置！");
